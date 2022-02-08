c7a26d197d0673ac33330ab98f94493f1d4a3614
David Tyler <david.tyler@metaswitch.com>
2022-02-08 14:50:35 +0000

    Added SDL2 based UI

---
+++ GameboyAdvanced.Sdl2/Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+using GameboyAdvanced.Core;
+using GameboyAdvanced.Core.Debug;
+using GameboyAdvanced.Core.Rom;
+using Serilog;
+
+namespace GameboyAdvanced.Sdl2;
+
+internal class Program
+{
+    public class Options
+    {
+        [Option('r', "rom", Required = true)]
+        public string Rom { get; }
+
+        [Option('b', "bios", Required = true)]
+        public string Bios { get; }
+
+        [Option("runBios", Default = false, HelpText = "Set --runBios to force the device to start at the RESET vector (0x0000_0000) instead of the first instruction on the gamepak")]
+        public bool RunBios { get; }
+
+        public Options(string rom, string bios, bool runBios)
+        {
+            Rom = rom ?? throw new ArgumentNullException(nameof(rom));
+            Bios = bios ?? throw new ArgumentNullException(nameof(bios));
+            RunBios = runBios;
+        }
+    }
+
+    internal static void Main(string[] args)
+    {
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            //.WriteTo.Console(outputTemplate: "{Message:lj}{NewLine}")
+            .WriteTo.File("log.txt", outputTemplate: "{Message:lj}{NewLine}")
+            .CreateLogger();
+
+        _ = Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(o =>
+            {
+                var bios = File.ReadAllBytes(o.Bios);
+                var rom = File.ReadAllBytes(o.Rom);
+
+                var gamepak = new GamePak(rom);
+                var device = new Device(bios, gamepak, new TestDebugger(), o.RunBios ? 0x0000_0000u : 0x0800_0000u);
+
+                var application = new Sdl2Application(device);
+                application.Run();
+            });
+    }
+}
---
+++ GameboyAdvanced.Sdl2/Sdl2Application.cs
@@ -0,0 +1,128 @@
+﻿using GameboyAdvanced.Core;
+using SDL2;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace GameboyAdvanced.Sdl2;
+
+internal class Sdl2Application : IDisposable
+{
+    private readonly Device _device;
+    private IntPtr _window;
+    private IntPtr _renderer;
+    private IntPtr _texture;
+    private readonly Stopwatch _stopwatch = new();
+    private bool _disposedValue;
+    private readonly int _msPerFrame = (int)(1.0 / 60 * 1000);
+
+    internal Sdl2Application(Device device)
+    {
+        _device = device;
+    }
+
+    private void SetupSdl2()
+    {
+        if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
+        {
+            throw new Exception($"Can't set up SDL video: {SDL.SDL_GetError()}");
+        }
+
+        _window = SDL.SDL_CreateWindow(
+            $"GBA - {_device.LoadedRomName()}",
+            SDL.SDL_WINDOWPOS_UNDEFINED,
+            SDL.SDL_WINDOWPOS_UNDEFINED,
+            240,
+            160,
+            SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+        if (_window == IntPtr.Zero)
+        {
+            throw new Exception($"Can't set up SDL window: {SDL.SDL_GetError()}");
+        }
+
+        _renderer = SDL.SDL_CreateRenderer(
+            _window,
+            -1,
+            SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+        if (_renderer == IntPtr.Zero)
+        {
+            throw new Exception($"Can't set up SDL renderer: {SDL.SDL_GetError()}");
+        }
+
+        _texture = SDL.SDL_CreateTexture(
+            _renderer,
+            SDL.SDL_PIXELFORMAT_ABGR8888,
+            (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
+            Device.WIDTH,
+            Device.HEIGHT);
+    }
+
+    internal void Run()
+    {
+        SetupSdl2();
+        _stopwatch.Start();
+        var running = true;
+
+        while (running)
+        {
+            while (SDL.SDL_PollEvent(out SDL.SDL_Event e) == 1)
+            {
+                switch (e.type)
+                {
+                    case SDL.SDL_EventType.SDL_QUIT:
+                        running = false;
+                        break;
+                    // TODO - Not handling inputs
+                }
+            }
+
+            // TODO - input latency is one frame if we clock a single frame before checking before events
+            _device.RunFrame();
+
+            var frameBuffer = _device.GetFrame();
+
+            unsafe
+            {
+                fixed (byte* p = frameBuffer)
+                {
+                    _ = SDL.SDL_UpdateTexture(_texture, IntPtr.Zero, (IntPtr)p, Device.WIDTH * 4);
+                }
+            }
+
+            _ = SDL.SDL_RenderCopy(_renderer, _texture, IntPtr.Zero, IntPtr.Zero);
+            SDL.SDL_RenderPresent(_renderer);
+
+            var msToSleep = _msPerFrame - (_stopwatch.ElapsedTicks / (double)Stopwatch.Frequency * 1000);
+            Console.WriteLine(msToSleep);
+            if (msToSleep > 0)
+            {
+                SDL.SDL_Delay((uint)msToSleep);
+            }
+            _stopwatch.Restart();
+        }
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (_texture != IntPtr.Zero) SDL.SDL_DestroyTexture(_texture);
+            if (_renderer != IntPtr.Zero) SDL.SDL_DestroyRenderer(_renderer);
+            if (_window != IntPtr.Zero) SDL.SDL_DestroyWindow(_window);
+            SDL.SDL_Quit();
+            _disposedValue = true;
+        }
+    }
+
+    ~Sdl2Application()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
