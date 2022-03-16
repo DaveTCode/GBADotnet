24d16868508810148b170b6e41499262ae966136
David Tyler <david.tyler@metaswitch.com>
2022-03-16 10:41:13 +0000

    Added comments to the background control register

--- GameboyAdvanced.Core/Ppu/Registers/BgControlReg.cs
+++ GameboyAdvanced.Core/Ppu/Registers/BgControlReg.cs
@@ -1,14 +1,53 @@
 ﻿namespace GameboyAdvanced.Core.Ppu.Registers;
 
+internal enum BgSize
+{
+    Regular32x32 = 0b00,
+    Regular64x32 = 0b01,
+    Regular32x64 = 0b10,
+    Regular64x64 = 0b11,
+}
+
 internal struct BgControlReg
 {
+    /// <summary>
+    /// Priority of BG against sprites and other BGs. highest priority is 
+    /// _drawn_ first so 0 will appear on top of all except sprites with 
+    /// priority 0.
+    /// </summary>
     internal int BgPriority;
+
+    /// <summary>
+    /// Where are the tiles for this BG stored? CharBaseBlock * 16KB
+    /// </summary>
     internal int CharBaseBlock;
+
+    /// <summary>
+    /// Is mosaic enabled for this background (depending on value of MOSAIC 
+    /// register)
+    /// </summary>
     internal bool IsMosaic;
+
+    /// <summary>
+    /// Are tiles using 4bpp (false) or 8bpp (true)
+    /// </summary>
     internal bool LargePalette;
+
+    /// <summary>
+    /// Where is the tile map for this BG stored? ScreenBaseBlock * 2KB
+    /// </summary>
     internal int ScreenBaseBlock;
+
+    /// <summary>
+    /// Only valid on affine backgrounds, after affine transform do pixels 
+    /// wrap around or become transparent?
+    /// </summary>
     internal bool DisplayAreaOverflow;
-    internal int ScreenSize;
+
+    /// <summary>
+    /// How many screen blocks (32*32) areas are in the tilemap
+    /// </summary>
+    internal BgSize ScreenSize;
 
     internal ushort Read() => (ushort)
         (BgPriority |
@@ -18,7 +57,7 @@ internal struct BgControlReg
          (LargePalette ? 1 << 7 : 0) |
          (ScreenBaseBlock << 8) |
          (DisplayAreaOverflow ? 1 << 13 : 0) |
-         (ScreenSize << 14));
+         ((ushort)ScreenSize << 14));
 
     internal void Update(ushort value)
     {
@@ -28,6 +67,6 @@ internal struct BgControlReg
         LargePalette = ((value >> 7) & 0b1) == 0b1;
         ScreenBaseBlock = (value >> 8) & 0b1_1111;
         DisplayAreaOverflow = ((value >> 13) & 0b1) == 0b1;
-        ScreenSize = (value >> 14) & 0b11;
+        ScreenSize = (BgSize)((value >> 14) & 0b11);
     }
 }
