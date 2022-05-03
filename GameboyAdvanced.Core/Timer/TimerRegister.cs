bffaf5f7763a4505efe21620f9208e590ada6387
David Tyler <david.tyler@metaswitch.com>
2022-05-03 14:20:01 +0100

    Count up timing doesn't need calculations, it's just the last value set during an overflow event

--- GameboyAdvanced.Core/Timer/TimerRegister.cs
+++ GameboyAdvanced.Core/Timer/TimerRegister.cs
@@ -35,10 +35,13 @@ public unsafe class TimerRegister
 
     internal ushort ReadCounter()
     {
-        if (StartLatch)
+        // Timers which are stopped or timers which use countup timing don't
+        // need calculating as the counter value is fixed from the last time
+        // it was set
+        if (StartLatch && !CountUpTimingLatch)
         {
-            var cycleDiff = _device.Cpu.Cycles - CyclesAtLastLatch;
-            var increments = cycleDiff / PrescalerSelectionLatch.Cycles();
+            var cyclesSinceLastLatch = _device.Cpu.Cycles - CyclesAtLastLatch;
+            var increments = cyclesSinceLastLatch / PrescalerSelectionLatch.Cycles();
             increments -= (CounterAtLastLatch - ReloadLatch);
 
             return (ushort)(ReloadLatch + (increments % (0x1_0000 - ReloadLatch)));
@@ -97,34 +100,22 @@ public unsafe class TimerRegister
         timer.LatchValues(ref device.TimerController._timerSteps[ix]);
     }
 
-    internal static void LatchTimer0ValuesEvent(Device device)
-    {
-        LatchTimerValuesEvent(device, 0);
-    }
+    internal static void LatchTimer0ValuesEvent(Device device) => LatchTimerValuesEvent(device, 0);
 
-    internal static void LatchTimer1ValuesEvent(Device device)
-    {
-        LatchTimerValuesEvent(device, 1);
-    }
+    internal static void LatchTimer1ValuesEvent(Device device) => LatchTimerValuesEvent(device, 1);
 
-    internal static void LatchTimer2ValuesEvent(Device device)
-    {
-        LatchTimerValuesEvent(device, 2);
-    }
+    internal static void LatchTimer2ValuesEvent(Device device) => LatchTimerValuesEvent(device, 2);
 
-    internal static void LatchTimer3ValuesEvent(Device device)
-    {
-        LatchTimerValuesEvent(device, 3);
-    }
+    internal static void LatchTimer3ValuesEvent(Device device) => LatchTimerValuesEvent(device, 3);
 
-    private static void TimerOverflowEvent(Scheduler scheduler, TimerController timerController, InterruptInterconnect interruptInterconnect, int ix)
+    private static void TimerOverflowEvent(Device device, int ix)
     {
-        timerController._timerSteps[ix] = timerController._timers[ix].PrescalerSelectionLatch.Cycles();
-        var timer = timerController._timers[ix];
+        device.TimerController._timerSteps[ix] = device.TimerController._timers[ix].PrescalerSelectionLatch.Cycles();
+        var timer = device.TimerController._timers[ix];
 
         if (timer.IrqEnabledLatch)
         {
-            interruptInterconnect.RaiseInterrupt(ix switch
+            device.InterruptInterconnect.RaiseInterrupt(ix switch
             {
                 0 => Interrupt.Timer0Overflow,
                 1 => Interrupt.Timer1Overflow,
@@ -134,45 +125,53 @@ public unsafe class TimerRegister
             });
         }
 
+        // Handle count up timers by checking if the next timer is both
+        // counting up _and_ started
         if (ix < 3)
         {
-            if (timerController._timers[ix + 1].CountUpTimingLatch)
+            if (device.TimerController._timers[ix + 1].CountUpTimingLatch && device.TimerController._timers[ix + 1].StartLatch)
             {
-                TimerOverflowEvent(scheduler, timerController, interruptInterconnect, ix + 1);
+                device.TimerController._timers[ix + 1].CounterAtLastLatch++;
+                device.TimerController._timers[ix + 1].CyclesAtLastLatch = device.Cpu.Cycles;
+
+                if (device.TimerController._timers[ix + 1].CounterAtLastLatch == 0)
+                {
+                    TimerOverflowEvent(device, ix + 1);
+                }
             }
         }
 
-        var cyclesToOverflow = timer.PrescalerSelectionLatch.Cycles() * (0xFFFF - timer.ReloadLatch);
+        var cyclesToOverflow = timer.PrescalerSelectionLatch.Cycles() * (0x1_0000 - timer.ReloadLatch);
 
-        scheduler.ScheduleEvent(OverflowEventTypes[ix], OverflowEvents[ix], cyclesToOverflow);
+        if (!timer.CountUpTimingLatch)
+        {
+            device.Scheduler.ScheduleEvent(OverflowEventTypes[ix], OverflowEvents[ix], cyclesToOverflow);
+        }
     }
 
-    internal static void Timer0OverflowEvent(Device device)
-    {
-        TimerOverflowEvent(device.Scheduler, device.TimerController, device.InterruptInterconnect, 0);
-    }
+    internal static void Timer0OverflowEvent(Device device) => TimerOverflowEvent(device, 0);
 
-    internal static void Timer1OverflowEvent(Device device)
-    {
-        TimerOverflowEvent(device.Scheduler, device.TimerController, device.InterruptInterconnect, 1);
-    }
+    internal static void Timer1OverflowEvent(Device device) => TimerOverflowEvent(device, 1);
 
-    internal static void Timer2OverflowEvent(Device device)
-    {
-        TimerOverflowEvent(device.Scheduler, device.TimerController, device.InterruptInterconnect, 2);
-    }
+    internal static void Timer2OverflowEvent(Device device) => TimerOverflowEvent(device, 2);
 
-    internal static void Timer3OverflowEvent(Device device)
-    {
-        TimerOverflowEvent(device.Scheduler, device.TimerController, device.InterruptInterconnect, 3);
-    }
+    internal static void Timer3OverflowEvent(Device device) => TimerOverflowEvent(device, 3);
 
     internal void UpdateControl(ushort value)
     {
         PrescalerSelection = (TimerPrescaler)(value & 0b11);
         CountUpTiming = (value & (1 << 2)) == (1 << 2);
         IrqEnabled = (value & (1 << 6)) == (1 << 6);
+        var oldStart = Start;
         Start = (value & (1 << 7)) == (1 << 7);
+
+        if (Start && !oldStart)
+        {
+            CounterAtLastLatch = Reload;
+            // Add 2 here as the timer doesn't start for 2 cycles after writing
+            // (1 for latch and one for start delay)
+            CyclesAtLastLatch = _device.Cpu.Cycles + 2; 
+        }
 
         _device.Scheduler.ScheduleEvent(LatchEventTypes[Index], LatchEvents[Index], 1);
     }
@@ -199,31 +198,28 @@ public unsafe class TimerRegister
     /// </summary>
     internal void LatchValues(ref int timerSteps)
     {
-        // Cache off the current known counter value before changing latches
-        // so we can read it as needed
-        CounterAtLastLatch = ReadCounter();
-        CyclesAtLastLatch = _device.Cpu.Cycles;
-
         ReloadLatch = Reload;
         CountUpTimingLatch = CountUpTiming;
         IrqEnabledLatch = IrqEnabled;
         PrescalerSelectionLatch = PrescalerSelection;
         timerSteps = PrescalerSelectionLatch.Cycles();
-        StartLatch = Start;
 
-        var cyclesToOverflow = PrescalerSelectionLatch.Cycles() * (0xFFFF - ReloadLatch);
-
-        if (StartLatch)
+        if (Start && !CountUpTimingLatch)
         {
+            var cyclesToOverflow = PrescalerSelectionLatch.Cycles() * (0x1_0000 - ReloadLatch);
+
             // Any latch causes us to recalculate the cycles until the timer
             // overflows, this call reschedules the event for the new number
             // of cycles
             _device.Scheduler.ScheduleEvent(OverflowEventTypes[Index], OverflowEvents[Index], cyclesToOverflow);
-            CounterAtLastLatch = ReloadLatch;
         }
         else
         {
+            CounterAtLastLatch = ReadCounter();
+            CyclesAtLastLatch = _device.Cpu.Cycles;
             _device.Scheduler.CancelEvent(OverflowEventTypes[Index]);
         }
+
+        StartLatch = Start;
     }
 }
